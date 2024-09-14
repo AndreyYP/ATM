3771fcdb8ebce9e80ae5b8ca39a5070966f61325
AndreyYP <wort912@yandex.ru>
2024-09-14 18:53:04 +0500

    initilization server

---
+++ ATM10/kubejs/server_scripts/modpack/att_items.js
@@ -0,0 +1,100 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+  // Dragon Soul
+  allthemods.shaped('allthetweaks:dragon_soul', ['CDA', 'SNI', 'BGE'], {
+    C: 'apothic_enchanting:infused_breath',
+    D: 'occultism:soul_gem',
+    A: 'occultism:spawn_egg/familiar_dragon',
+    S: 'productivetrees:socotra_dragon_sapling',
+    N: 'hostilenetworks:data_model[hostilenetworks:data_model="hostilenetworks:ender_dragon",hostilenetworks:data=1254]',
+    I: 'allthemodium:piglich_heart_block',
+    B: 'productivebees:spawn_egg_configurable_bee[entity_data={id:"productivebees:configurable_bee",type:"productivebees:soul_lava"}]',
+	  G: 'productivebees:inactive_dragon_egg',
+    E: 'eternal_starlight:chain_of_souls'
+  }).id('allthemods:allthetweaks/dragon_soul')
+
+ // Improbable Probability Device
+ allthemods.shaped('allthetweaks:improbable_probability_device', ['ABA', 'CGE', 'FDF'], {
+  A: 'mekanism:pellet_antimatter',
+  B: 'ae2:singularity',
+  C: 'megacells:portable_item_cell_256m',
+  D: 'pneumaticcraft:aerial_interface',
+  E: 'modern_industrialization:blastproof_casing',
+  F: 'ironfurnaces:million_furnace', 
+  G: 'irons_spellbooks:lightning_upgrade_orb'
+  }).id('allthemods:allthetweaks/improbable_probability_device')
+
+  // Dimensional Seed Recipe
+  allthemods.shaped('allthetweaks:dimensional_seed', ['ABC', 'DEF', 'GHI'], {
+    A: 'allthecompressed:netherrack_6x',
+    B: 'allthecompressed:dirt_6x',
+    C: 'allthecompressed:obsidian_5x',
+    D: 'allthetweaks:mini_exit',
+    E: 'allthetweaks:mini_nether',
+    F: 'allthetweaks:mini_end',
+    G: 'allthecompressed:end_stone_5x',
+    H: 'allthecompressed:emerald_block_4x',
+    I: 'allthecompressed:diamond_block_4x'
+  }).id('allthemods:allthetweaks/dimensional_seed')
+
+    // Withers Compass
+    allthemods.shaped('allthetweaks:withers_compass', ['DCD', 'ABA', 'DED'], {
+      A: 'minecraft:bedrock',
+      B: 'minecraft:bedrock',
+      C: 'deeperdarker:heart_of_the_deep',
+      D: 'minecraft:bedrock',
+      E: 'minecraft:bedrock'
+    }).id('allthemods:allthetweaks/withers_compass')
+
+  // Philosopher's Fuel
+  allthemods.shaped('allthetweaks:philosophers_fuel', ['ABC', 'DEF', 'GHI'], {
+  A: 'generatorgalore:ender_generator',
+	B: 'ironfurnaces:million_furnace'  ,
+	C: 'bigreactors:insanite_block' ,
+	D: 'minecraft:bedrock',
+	E: 'mysticalagradditions:insanium_coal_block',
+  F: 'minecraft:bedrock',
+	G: 'mysticalagriculture:awakened_supremium_ingot_block',
+	H: 'minecraft:bedrock',
+  I: 'minecraft:bedrock'
+  }).id('allthemods:allthetweaks/philosophers_fuel')
+
+ // Nexium Emitter
+ allthemods.shaped('allthetweaks:nexium_emitter', ['A B', ' CF', 'GED'], {
+  A: 'powah:player_transmitter_nitro',
+  B: 'minecraft:bedrock',
+  F: [Item.of('mekanism:module_gravitational_modulating_unit')],
+  D: 'minecraft:bedrock',
+  E: 'ae2:singularity',
+  C: 'minecraft:bedrock',
+  G: 'minecraft:bedrock'
+}).id('allthemods:allthetweaks/nexium_emitter')
+
+// Oblivion Shard
+allthemods.shaped('allthetweaks:oblivion_shard', [' AB', 'ACA', 'BA '], {
+  A: 'minecraft:bedrock',
+  C: 'minecraft:bedrock',
+  B: 'evilcraft:piercing_vengeance_focus'
+}).id('allthemods:allthetweaks/oblivion_shard')
+
+
+// Pulsating Black Hole
+allthemods.shaped('allthetweaks:pulsating_black_hole', ['ABC', 'DEF', 'GHI'], {
+  A: 'irons_spellbooks:fire_upgrade_orb',
+  B: 'ae2:quantum_ring'  ,
+  C: 'pneumaticcraft:micromissiles' ,
+  D: 'minecraft:bedrock',
+  E: 'minecraft:bedrock',
+  F: 'occultism:stable_wormhole',
+  G: 'minecraft:bedrock',
+  H: 'minecraft:bedrock',
+  I: 'evilcraft:lightning_bomb'
+}).id('allthemods:pressure/allthetweaks/pulsating_black_hole')
+
+})
+
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
